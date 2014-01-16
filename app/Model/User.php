bc3274776389ec0710728d99e9f139aa93a38d07
YukiTETSUKA <yuki@tedzukayuuki-no-MacBook-Air.local>
2014-01-16 09:17:17 +0900

    Model/User.php更新

--- app/Model/User.php
+++ app/Model/User.php
@@ -31,4 +31,23 @@
 
             return $data;
         }
+
+        public function facebook_sign_up($token){
+            if(is_string($token)){
+                return;
+            }
+
+            $user = $token->api('/me');
+
+            $data['name']        = $user['username'];
+            $data['password']    = Security::hash($user['username']);
+            $data['provider']    = 'facebook';
+            $data['provider_id'] = $user['id'];
+
+            if($this->validates()){
+                if(!$this->save($data) && !$this->find('first', array('conditions' => array('provider' => $data['provider'], 'provider_id' => $data['provider_id'])))){
+                    return;
+                }
+            }
+        }
     }
